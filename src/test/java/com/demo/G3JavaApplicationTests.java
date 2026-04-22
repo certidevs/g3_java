9bd07984a7a48c4b8806a2ab0ed04f614e5ad708
alansastre <alan@certidevs.com>
2026-04-22 12:01:14 +0200

    Proyecto setup

---
+++ src/test/java/com/demo/G3JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G3JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
