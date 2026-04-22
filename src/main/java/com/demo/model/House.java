e1ae1e1ae3f6121cb34c6c867b3c470690ca0f15
NALA <angelica.giraldo10699@gmail.com>
2026-04-22 12:10:27 +0200

    Creacion de paq Model y Entidad House

---
+++ src/main/java/com/demo/model/House.java
@@ -0,0 +1,28 @@
+package com.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class House {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private  String description;
+    private Double pricePerNight;
+    private String location;
+    private Integer maxGuests;
+//Constructor
+    public House() {
+    }
+
+
+}
