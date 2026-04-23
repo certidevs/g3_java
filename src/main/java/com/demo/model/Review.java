20577c2a58e65b0a8d882463d8b7190e30031f7f
Ángel García Beltrán <angelurano00@gmail.com>
2026-04-23 10:44:31 +0200

    Use lombok annotations for class Review #4, instead of constructors, getters, setters, etc

--- src/main/java/com/demo/model/Review.java
+++ src/main/java/com/demo/model/Review.java
@@ -1,17 +1,20 @@
 package com.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "review")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class Review {
     @Id
     @Column(name = "id", nullable = false)
@@ -26,11 +29,4 @@ public class Review {
     @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
 
-    public Review() {}
-
-    public Review(Integer rating, String comment, LocalDateTime createdAt) {
-        this.rating = rating;
-        this.comment = comment;
-        this.createdAt = createdAt;
-    }
 }
