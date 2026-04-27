1e52be376f1b5e24db98b9ae4f565a958da615c2
angelica giraldo <angelica.giraldo10699@gmail.com>
2026-04-27 11:35:11 +0200

    FILTRO EN HouseRepository

--- src/main/java/com/demo/repository/HouseRepository.java
+++ src/main/java/com/demo/repository/HouseRepository.java
@@ -3,6 +3,23 @@ package com.demo.repository;
 import com.demo.model.House;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HouseRepository extends JpaRepository<House, Long> {
+
+    //FILTRAR POR UBICACION DE CASA
+
+    List<House> findByLocation(String location);
+
+    //FILTRAR POR ID
+
+    List<House> findByIdOrderByIdAsc(Long id);
+
+    //FILTRAR POR PRECIO POR NOCHE
+
+    List<House> findByPricePerNightBetween(Double pricePerNightStart, Double pricePerNightEnd);
+
+
+    //FILTRAR REVIEW CON MEJORES RESEÑAS
 
 }
