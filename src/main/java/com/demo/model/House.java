ed7b33d9c609c187be1f5a13a135ef886f98b1c3
Angelica <angelica.giraldo10699@gmail.com>
2026-04-29 11:19:28 +0200

    Navbar Modificado

--- src/main/java/com/demo/model/House.java
+++ src/main/java/com/demo/model/House.java
@@ -1,24 +1,31 @@
 package com.demo.model;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
 @Entity
 @Table(name = "houses")
+@AllArgsConstructor
 public class House {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    @Column(length = 1000)
     private  String description;
     private Double pricePerNight;
     private String location;
+    private String province;
     private Integer maxGuests;
+    @Builder.Default
     @Enumerated(EnumType.STRING)
-    private StatusReserva reserva = StatusReserva.DISPONIBLE;
+    private StatusReserva reserve = StatusReserva.DISPONIBLE;
 
     @ManyToOne
     private User host;
